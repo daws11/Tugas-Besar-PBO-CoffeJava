fbe6268f481857bb458c7a631dd5d4f418a60e04
kevin albany <135513743+KevZ12@users.noreply.github.com>
2024-06-01 19:21:49 +0700

    login register page (#1)

--- hospital/src/hospital/Hospital.java
+++ hospital/src/hospital/Hospital.java
@@ -3,17 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package hospital;
-import View.LoginPatientView;
-import controller.DoctorController;
-import java.sql.Connection;
-import java.sql.DriverManager;
 
-import database.DataBaseConnection;
-import java.sql.SQLException;
-import controller.PatientController;
-import java.security.NoSuchAlgorithmException;
-import view.*;
-import util.PasswordUtil;
 
 
 /**
@@ -25,25 +15,7 @@ public class Hospital {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws NoSuchAlgorithmException {
-        PatientController patientController = new PatientController();
-        DoctorController doctorController = new DoctorController();
-        
-        RegisterPatientView registerPatientView =new RegisterPatientView(patientController);
-        //LoginPatientView loginPatientView = new LoginPatientView(patientController);
-        //loginPatientView.setVisible(true);
-        registerPatientView.setVisible(true);
-        
-        //LoginDoctorView loginDoctorView =  new LoginDoctorView();
-        //loginDoctorView.setVisible(true);
-        //String password = "y8YKzJJBkmmm4H3ctRkwGRnIxgI4znjVAe/C6y/7MaW+WwXu9IFhtn40LvHQmiZqp2/Hnm7cvturmxtfS/7reA==";
-        //String salt = "IqBt6cBEpPV0WMG3BI5nvg==";
-        //String hashPassword = PasswordUtil.hashPassword(password, salt);
-        
-        //System.out.println(salt);
-        //System.out.println(hashPassword);
-        
-        //System.out.println(PasswordUtil.comparePassword(password, hashPassword, salt));
+    public static void main(String[] args) {
         
         
         
