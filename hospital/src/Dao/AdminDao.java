296bb1fbc87ab048328361b8293169bad68d43fc
KevZ12 <135513743+KevZ12@users.noreply.github.com>
2024-06-02 17:24:45 +0700

    menambahkan semua fungsionalitas controller admin, patient, doctor

--- hospital/src/Dao/AdminDao.java
+++ hospital/src/Dao/AdminDao.java
@@ -4,14 +4,20 @@
  */
 package Dao;
 
+import controller.SpecializationController;
 import database.DataBaseConnection;
 import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 import model.Admin;
+import model.Doctor;
+import model.Specialization;
 import util.PasswordUtil;
 
 /**
@@ -66,9 +72,123 @@ public class AdminDao {
         
     }
     
-    //add doctor
+    public boolean editAdmin(int adminId, String firstName, String lastName, String email, String phoneNumber) throws SQLException {
+        String query = "UPDATE admins SET FirstName = ?, LastName = ?, Email = ?, PhoneNumber = ? WHERE AdminId = ?";
+        try (Connection connection = DataBaseConnection.getConnection(); PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, firstName);
+            statement.setString(2, lastName);
+            statement.setString(3, email);
+            statement.setString(4, phoneNumber);
+            
+            statement.setInt(5, adminId);
+            return statement.executeUpdate() > 0;
+        }
+    }
+
+    // Delete an admin
+    public boolean deleteAdmin(int adminId) throws SQLException {
+        String query = "DELETE FROM admins WHERE AdminId = ?";
+        try (Connection connection = DataBaseConnection.getConnection(); PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, adminId);
+            return statement.executeUpdate() > 0;
+        }
+    }
+    
+    public boolean addDoctor(Doctor doctor) throws SQLException {
+        String query = "INSERT INTO doctors (FirstName, LastName, SpecializationId, Address, BirthDate,PhoneNumber,Email,Password,Salt) VALUES (?, ?, ?, ?, ?, ?,?,?,?)"; 
+        try (Connection connection = DataBaseConnection.getConnection(); PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, doctor.getFirstName());
+            statement.setString(2, doctor.getLastName());
+            statement.setInt(3, doctor.getSpecialization().getSpecializationId());
+            statement.setString(4, doctor.getAddress());
+            statement.setDate(5, java.sql.Date.valueOf(doctor.getBirtDate()));
+            statement.setString(6, doctor.getPhoneNumber());
+            statement.setString(7, doctor.getEmail());
+            statement.setString(8, doctor.getPassword());
+            statement.setString(9, doctor.getSalt());
+             return statement.executeUpdate() > 0;
+        }
+    }
+    
+    
     //delete doctors
+    public boolean deleteDoctor(int doctorId) throws SQLException {
+    String query = "DELETE FROM doctors WHERE DoctorId = ?";
+    try (Connection connection = DataBaseConnection.getConnection(); PreparedStatement statement = connection.prepareStatement(query)) {
+        statement.setInt(1, doctorId);
+        return statement.executeUpdate() > 0;
+    }
+}
+
     //edit doctor
+    
+    public boolean updateDoctor(Doctor doctor) throws SQLException {
+    String query = "UPDATE doctors SET FirstName = ?, LastName = ?, Address = ?, BirthDate = ?, PhoneNumber = ?, Email = ? WHERE DoctorId = ?";
+    try (Connection connection = DataBaseConnection.getConnection(); PreparedStatement statement = connection.prepareStatement(query)) {
+        statement.setString(1, doctor.getFirstName());
+        statement.setString(2, doctor.getLastName());
+        statement.setString(3, doctor.getAddress());
+        statement.setDate(4, java.sql.Date.valueOf(doctor.getBirtDate()));
+        statement.setString(5, doctor.getPhoneNumber());
+        statement.setString(6, doctor.getEmail());
+        statement.setInt(7, doctor.getDoctorId());
+        return statement.executeUpdate() > 0;
+    }
+}
+    //getalldoctor
+    public List<Doctor> getAllDoctors() throws SQLException {
+     SpecializationController specializationController = new SpecializationController();
+    List<Doctor> doctors = new ArrayList<>();
+    String query = "SELECT * FROM doctors";
+    try (Connection connection = DataBaseConnection.getConnection(); PreparedStatement statement = connection.prepareStatement(query); ResultSet resultSet = statement.executeQuery()) {
+        while (resultSet.next()) {
+            int doctorId = resultSet.getInt("DoctorId");
+            String firstName = resultSet.getString("FirstName");
+            String lastName = resultSet.getString("LastName");
+            int specializationId = resultSet.getInt("SpecializationId");
+            String address = resultSet.getString("Address");
+            LocalDate birthDate = resultSet.getDate("BirthDate").toLocalDate();
+            String phoneNumber = resultSet.getString("PhoneNumber");
+            String email = resultSet.getString("Email");
+            String password = resultSet.getString("Password");
+            String salt = resultSet.getString("Salt");
+
+            Specialization specialization = specializationController.getSpecializationById(specializationId);
+
+            Doctor doctor = new Doctor(doctorId, address, birthDate, specialization, firstName, lastName, email, phoneNumber, password, salt);
+            doctors.add(doctor);
+        }
+    }
+    return doctors;
+}
+
+    
+    //getdoctorbyid
+    public Doctor getDoctorById(int doctorId) throws SQLException {
+        SpecializationController specializationController = new SpecializationController();
+    String query = "SELECT * FROM doctors WHERE DoctorId = ?";
+    try (Connection connection = DataBaseConnection.getConnection(); PreparedStatement statement = connection.prepareStatement(query)) {
+        statement.setInt(1, doctorId);
+        try (ResultSet resultSet = statement.executeQuery()) {
+            if (resultSet.next()) {
+                String firstName = resultSet.getString("FirstName");
+                String lastName = resultSet.getString("LastName");
+                int specializationId = resultSet.getInt("SpecializationId");
+                String address = resultSet.getString("Address");
+                LocalDate birthDate = resultSet.getDate("BirthDate").toLocalDate();
+                String phoneNumber = resultSet.getString("PhoneNumber");
+                String email = resultSet.getString("Email");
+                String password = resultSet.getString("Password");
+                String salt = resultSet.getString("Salt");
+
+                Specialization specialization = specializationController.getSpecializationById(specializationId);
+
+                return new Doctor(doctorId, address, birthDate, specialization, firstName, lastName, email, phoneNumber, password, salt);
+            }
+        }
+    }
+    return null;
+}
     
 
 }
--- hospital/src/hospital/Hospital.java
+++ hospital/src/hospital/Hospital.java
@@ -4,6 +4,8 @@
  */
 package hospital;
 
+import Dao.AdminDao;
+import controller.AdminController;
 import controller.DoctorController;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -12,6 +14,12 @@ import database.DataBaseConnection;
 import java.sql.SQLException;
 import controller.PatientController;
 import java.security.NoSuchAlgorithmException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import model.Admin;
+import model.Doctor;
+import model.Patient;
 
 import util.PasswordUtil;
 import view.SpecializationGUI;
@@ -27,8 +35,21 @@ public class Hospital {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws NoSuchAlgorithmException {
-
-        new SpecializationGUI().setVisible(true);
+        DoctorController doctorController = new DoctorController();
+        AdminController adminController = new AdminController();
+        PatientController patientController  = new PatientController();
+        try {
+            LocalDate birthDate = LocalDate.parse("2020-10-10");
+            boolean succ = patientController.deletePatient(22);
+            System.out.println("delete "+succ);
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+       
+                
         
         //PatientController patientController = new PatientController();
         //DoctorController doctorController = new DoctorController();
